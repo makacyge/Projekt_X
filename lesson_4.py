72f89ca5fa349c8e6f78b142c5a0834a2ea519eb
Admin <makacyge88@gmail.com>
2021-04-11 12:26:08 +0300

    add_Home_work_3.py

---
+++ lesson_4.py
@@ -0,0 +1,25 @@
+# www='www.conguer_and_command.net'
+# if ".com" in www:
+#     print("com in www")
+# else:
+#     print("com not in www")
+
+# v = 321
+# # m_s = str(v) if v < 200 else str(v)[::-1]
+# # print(m_s)
+
+# c = 10
+# e_f = True
+# while e_f:
+#     c -= 1
+#     if c > 0:
+#         e_f = False
+#         print("Test")
+
+# v = "123456789"
+# m_s = v if len(v)< 5 else v[2:5:2]
+# print(m_s)
+
+value = 120
+new_value = value / 2 if value < 100 else - value
+print(new_value)
